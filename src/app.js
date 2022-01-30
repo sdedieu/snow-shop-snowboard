238564df89a2ce1904c51eff754852b5601f0d89
sdedieu <dedieu.sylvain38@gmail.com>
2022-01-30 23:35:47 +0100

    setup

--- src/app.js
+++ src/app.js
@@ -1,43 +1,41 @@
-import angular from "angular";
-import "angular-route";
-import "./snowboard/snowboard";
-import "./snowboard-details/snowboard-details";
-import "./index.css";
+import angular from 'angular';
+import 'angular-route';
+import './snowboard/snowboard';
+import './snowboard-details/snowboard-details';
+import './index.css';
 
-angular
-  .module("app", ["ngRoute", "app.snowboard", "app.snowboardDetails"])
-  .component("app", {
-    controller: function ($location) {
-      this.message = "AngularJS";
-      this.version = require("../package.json").dependencies.angular;
-      this.navigateTo = function (route) {
-        $location.path(route);
-      };
-    },
-    template: `
+angular.module('app', [  
+  'ngRoute',
+'app.snowboard',
+'app.snowboardDetails',]).component('app', { 
+  controller: function ($location) {
+    this.message = 'AngularJS';
+    this.version = require('../package.json').dependencies.angular;
+    this.navigateTo = function(route){
+      $location.path(route)
+    }
+  },
+  template: `
   <div ng-view></div>
-  `,
-  })
-  .config([
-    "$locationProvider",
-    "$routeProvider",
-    function ($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix("!");
+  `
+}).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
 
-      $routeProvider.otherwise({ redirectTo: "/items" });
-    },
-  ]);
+  $routeProvider.otherwise({redirectTo: '/items'});
+}]);
+
+// Note: You can also wrap a controller or a directive 
+// in a Custom Element!
 
 /*
-export class Mfe extends HTMLElement {
-  connectedCallBack() {
-    const root = document.createElement("app");
+export class MfeAngularJs extends HTMLElement {
+  connectedCallback() {
+    const root = document.createElement('app');
     this.appendChild(root);
-    angular.bootstrap(root, ["app"]);
+    angular.bootstrap(root, ['app']);
   }
 }
-customElements.define("snowboard-web-component", Mfe);
-*/
 
-// Note: You can also wrap a controller or a directive
-// in a Custom Element!
+customElements.define('snowboard-web-component', MfeAngularJs);
+*/
